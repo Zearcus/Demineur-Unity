f7a53a3557483f44860d31a156cb15590bb34fb1
Fabien Marchand <ghost.fm65@gmail.com>
2023-03-01 16:51:42 +0100

    update

---
+++ Assets/Scripts/SizeGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeGame : MonoBehaviour
+{
+    public Game game;
+    public Toggle size8x8;
+    public Toggle size16x16;
+    public Toggle size32x32;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
